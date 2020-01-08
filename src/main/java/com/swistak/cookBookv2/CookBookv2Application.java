f3f32373351ef38d9ebd1a89a8219e4ed1e5fd34
Łukasz Matysik <lukasz.matysik6@gmail.com>
2020-01-08 22:09:05 +0100

    Initial commit

---
+++ src/main/java/com/swistak/cookBookv2/CookBookv2Application.java
@@ -0,0 +1,13 @@
+package com.swistak.cookBookv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookBookv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookBookv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/swistak/cookBookv2/CookBookv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swistak.cookBookv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookBookv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
